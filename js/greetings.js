d212412f6b835dc331295a2787ef6f8a61f169d1
root <partemissg@gmail.com>
2022-01-21 09:03:30 +0900

    finish

--- js/greetings.js
+++ js/greetings.js
@@ -1,6 +1,7 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const todo = document.querySelector("#todo-wrap");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -14,8 +15,9 @@ function onLoginSubmit(event) {
 }
 
 function paintGreetings(username) {
-  greeting.innerText = `Hello ${username}`;
+  greeting.innerText = `Welcome! ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
+  todo.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
--- js/weather.js
+++ js/weather.js
@@ -7,10 +7,10 @@ function onGeoOk(position) {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      const weather = document.querySelector("#weather span:first-child");
-      const city = document.querySelector("#weather span:last-child");
+      const weather = document.querySelector("#weather #tmp");
+      const city = document.querySelector("#weather #loc");
       city.innerText = data.name;
-      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}℃`;
     });
 }
 function onGeoError() {
